d429c2fcc68a372fba69764884a1340870d2ff9c
KamyarKazemi <151082088+KamyarKazemi@users.noreply.github.com>
2025-12-03 16:29:50 +0330

    01

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,23 @@
+import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-hydrateRoot(document.getElementById("root"), <App />);
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  // Hydrate the existing SSR markup
+  hydrateRoot(
+    rootElement,
+    <StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StrictMode>
+  );
+} else {
+  // Fallback for CSR-only rendering
+  console.warn("Root element is empty. No SSR content found.");
+}
